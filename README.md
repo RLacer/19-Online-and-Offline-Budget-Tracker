93bb1e618b396dda06ecba4f50e16025567a1b86
RLacer <r.a.lacer@hotmail.com>
2021-07-01 22:12:27 -0600

    updating cache

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 19-Online-and-Offline-Budget-Tracker
+test to push
--- public/service-worker.js
+++ public/service-worker.js
@@ -4,13 +4,13 @@
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./style.css",
-  "./js/idb.js",
-  "./js/index.js",
-  "./manifest.json",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png",
+  "/index.html",
+  "/styles.css",
+  "/js/idb.js",
+  "/js/index.js",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 // install
